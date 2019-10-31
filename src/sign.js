85638880fba541d6a0be760e5991c550fe1cb51c
Kevin Suarez <kdock994@gmail.com>
2019-10-31 13:21:51 -0400

    3

---
+++ src/sign.js
@@ -0,0 +1,24 @@
+const readlineSync = require("readline-sync");
+
+const tn = Number(readlineSync.question("\nEnter a number: "));
+
+if (!Number.isInteger(tn)) {
+    console.log("\nInvalid.\n")
+
+} else if (tn < (Number.MIN_SAFE_INTEGER)) {
+   console.log("\nInvalid.\n")
+
+} else if (tn > (Number.MAX_SAFE_INTEGER)) {
+
+} else if (tn < 0) {
+   console.log("\nNegative.\n")
+
+} else if (tn == 0) {
+   console.log("\nZero.\n")
+
+} else if (theNumber > 0) {
+   console.log("\nPositive.\n")
+
+} else {
+   console.log("\nInvalid.\n")
+}
